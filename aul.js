5871bce58bf91e7a64052e79667763a240dab4a3
uliafauziah <uliafauziah896@gmail.com>
2024-05-13 07:12:49 +0700

    Add files via upload

---
+++ aul.js
@@ -0,0 +1,16 @@
+function sapaan() {
+    document.getElementById("sapaan").scrollIntoView();
+}
+function profile() {
+    document.getElementById("profile").scrollIntoView();
+}
+function aboutme() {
+    document.getElementById("aboutme").scrollIntoView();
+}
+
+function skil() {
+    document.getElementById("skil").scrollIntoView();
+}
+function kontak() {
+    document.getElementById("kontak").scrollIntoView();
+}
